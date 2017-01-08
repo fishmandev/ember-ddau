8b42eca413c91de73fe9cec0f61f4d83d8209fd1
Dmitriy Fishman <fishman.code@gmail.com>
2017-01-08 16:43:57 +0000

    Implement 'Data Down Actions Up'

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return [
+      {id: '1', label: 'item 1'},
+      {id: '2', label: 'item 2'},
+      {id: '3', label: 'item 3'},
+    ]
+  },
+  actions: {
+    saveRouteAction(item) {
+      //TODO Implement model saving here
+      Ember.Logger.info(item);
+    }
+  }
 });
